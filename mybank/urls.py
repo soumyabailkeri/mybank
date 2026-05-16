3d09c29180b67929d7535eb4138ce3b4d9c963e8
Soumya Ramesh Bailkeri <soumya@Soumyas-MacBook-Air.local>
2026-05-16 14:50:36 +0530

    Day 2: JWT authentication added and protected endpoints

--- mybank/urls.py
+++ mybank/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.views import TransactionViewSet, TellerSessionViewSet
-
 
 router = DefaultRouter()
 router.register(r'transactions', TransactionViewSet)
@@ -11,4 +11,6 @@ router.register(r'teller-sessions', TellerSessionViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
